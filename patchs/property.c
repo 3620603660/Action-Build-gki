63b6e9b83a594d4f5af45d3ff6306ed6b1c56634
drivers <72736899+t521418@users.noreply.github.com>
2025-05-27 12:27:21 +0800

    Update property.c

--- patchs/property.c
+++ patchs/property.c
@@ -1,21 +1,14 @@
 diff --git a/drivers/of/property.c b/drivers/of/property.c
 --- a/drivers/of/property.c
 +++ b/drivers/of/property.c
-@@ -451,3 +451,6 @@
-int of_property_read_string(const struct device_node *np, const char *propname,
-const char **out_string)
-{
-const struct property *prop = of_find_property(np, propname, NULL);
-if (!prop)
-return -EINVAL;
-if (!prop->length)
-return -ENODATA;
-if (strnlen(prop->value, prop->length) >= prop->length)
-return -EILSEQ; 
-*out_string = prop->value;
-+ if (strcmp(propname, "type") == 0 && strcmp(prop->value, "HMBIRD_OGKI") == 0) {
-+ *out_string = "HMBIRD_GKI";
-+ }
-return 0;
-}
-EXPORT_SYMBOL_GPL(of_property_read_string);
+@@ -459,6 +459,10 @@
+ 	if (strnlen(prop->value, prop->length) >= prop->length)
+ 		return -EILSEQ;
+ 	*out_string = prop->value;
++
++	if (strcmp(propname, "type") == 0 && strcmp(prop->value, "HMBIRD_OGKI") == 0)
++		*out_string = "HMBIRD_GKI";
++
+ 	return 0;
+ }
+ EXPORT_SYMBOL_GPL(of_property_read_string);
